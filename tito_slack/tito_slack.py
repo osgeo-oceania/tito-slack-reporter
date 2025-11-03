b5951fa2e584081e900a08109b55507d66af43f0
Alex Leith <alex@auspatious.com>
2025-11-03 14:11:01 +1100

    Add more info to summary, further split out workshops

--- tito_slack/tito_slack.py
+++ tito_slack/tito_slack.py
@@ -23,6 +23,7 @@ NON_WORKSHOP_ACTIVITIES = (
     "Women in Geospatial Breakfast",
     "Business to Business",
     "TGP Breakfast",
+    "Auckland Transport HOP card",
 )
 
 GROUPED = {
@@ -30,7 +31,54 @@ GROUPED = {
     "Conference Day 2": "Conference and Icebreaker",
     "Conference Day 3": "Conference and Icebreaker",
     "Icebreaker": "Conference and Icebreaker",
+    # Monday morning
+    "Advanced PostGIS: Beyond the basics Workshop": "Workshops (Monday morning)",
+    "Building Geospatial AI Applications: From Data to Insights with Open Source Tools Workshop": "Workshops (Monday morning)",
+    "Build the Thing: A Hands-On Product Workshop for Geospatial Makers Workshop": "Workshops (Monday morning)",
+    "Cloud-Native Geospatial for Earth Observation Workshop": "Workshops (Monday morning)",
+    "Diving into pygeoapi Workshop": "Workshops (Monday morning)",
+    "Exploring the ZOO-Project-DRU and OGC Application Package Workshop": "Workshops (Monday morning)",
+    "Hands-on DGGS and OGC DGGS-API with DGGRID and pydggsapi Workshop": "Workshops (Monday morning)",
+    "Introduction to GeoServer Workshop": "Workshops (Monday morning)",
+    "Let’s create Interactive Web Maps with the Open-Source WebGIS: Re:Earth Visualizer + Re:Earth CMS Workshop": "Workshops (Monday morning)",
+    "QField & QFieldCloud: Hands-On Fieldwork Workshop": "Workshops (Monday morning)",
+    # Monday afternoon
+    "Cartography for Rebels: Building Beautiful Maps with Free Tools Workshop": "Workshops (Monday afternoon)",
+    "Doing Geospatial with Python Workshop": "Workshops (Monday afternoon)",
+    "Exploring Cloud-Native Geospatial Formats: Hands-on with Raster Data Workshop": "Workshops (Monday afternoon)",
+    "GeoTools Geospatial Introduction Workshop": "Workshops (Monday afternoon)",
+    "Getting Sentinel Data within Seconds with STAC Workshop": "Workshops (Monday afternoon)",
+    "International QField Day Workshop": "Workshops (Monday afternoon)",
+    "Scalable Remote Sensing Workflows with Xarray Workshop": "Workshops (Monday afternoon)",
+    "QGIS Graphical Modeler: Build Smarter Workflows with Algorithms and Expressions Workshop": "Workshops (Monday afternoon)",
+    "QGIS Plugin Development": "Workshops (Monday afternoon)",
+    "Tile serving with MapLibre/Martin/Planetiler - base and overlays Workshop": "Workshops (Monday afternoon)",
+    # Tuesday morning
+    "Cloud-based Remote Sensing with QGIS and Google Earth Engine Workshop": "Workshops (Tuesday morning)",
+    "Exploring Cloud-Native Geospatial Formats: Hands-on with Vector Data Workshop": "Workshops (Tuesday morning)",
+    "FOSS4G with .NET: A Hands-On Introduction for Spatial Developers Workshop": "Workshops (Tuesday morning)",
+    "GeoServer 3 Developers Workshop": "Workshops (Tuesday morning)",
+    "Introduction to QField plugin authoring Workshop": "Workshops (Tuesday morning)",
+    "Modelling Climate Risks Using NASA Earthdata Cloud & Python APIs Workshop": "Workshops (Tuesday morning)",
+    "Oxidize to Decarbonize. Building more sustainable geospatial processes with Rust Workshop": "Workshops (Tuesday morning)",
+    "pgRouting basic Workshop": "Workshops (Tuesday morning)",
+    "Standalone Web Maps, No Platform Required Workshop": "Workshops (Tuesday morning)",
+    "Terra Draw - cross-platform drawing library for all map applications Workshop": "Workshops (Tuesday morning)",
+    # Tuesday afternoon
+    "Building Spatial APIs in PostgreSQL with PostgREST Workshop": "Workshops (Tuesday afternoon)",
+    "Collaborative Geospatial Workflows in Action: A Hands-On Alpha with Re:Earth Flow Workshop": "Workshops (Tuesday afternoon)",
+    "Create and Customize Your Own 3D Web Maps with TerriaJS Workshop": "Workshops (Tuesday afternoon)",
+    "Getting Started with GeoNetwork 5: A Hands-On Developer Workshop": "Workshops (Tuesday afternoon)",
+    "H-O-T-T-O-G-O: Mobile Apps That Support Disaster Response and Climate Resilience Workshop": "Workshops (Tuesday afternoon)",
+    "Open Data, Open Source, Open Standard: Quickly build your digital twin city with mago3D Workshop": "Workshops (Tuesday afternoon)",
+    "Participatory mapping field survey and computer lab: QField integration into machine learning landcover classification within Digital Earth Pacific Workshop": "Workshops (Tuesday afternoon)",
+    "Running and Auto Scaling Geoserver and PostgreSQL/PostGIS without managing servers in the AWS cloud Workshop": "Workshops (Tuesday afternoon)",
+    "Semantic Interoperability made easy with OGC Building Blocks Workshop": "Workshops (Tuesday afternoon)",
+    "Simulating Sustainable Urban Spaces on AWS Workshop": "Workshops (Tuesday afternoon)",
+    "The Deep Magic of QGIS Cartography Workshop": "Workshops (Tuesday afternoon)",
 }
+
+KNOWN_ACTIVITIES = set(NON_WORKSHOP_ACTIVITIES + tuple(GROUPED.keys()))
 
 
 def get_tito_tickets(
@@ -84,31 +132,37 @@ def get_tito_activities(
     activities = response.json()
 
     summary = {
-        "Workshops": 0,
+        "Other": 0,
     }
-    workshop_activity_names = set()
+    unknown_activity_names = set()
     for activity in activities["activities"]:
-        if activity["name"] in NON_WORKSHOP_ACTIVITIES:
+        if activity["name"] in KNOWN_ACTIVITIES:
             if activity["name"] in GROUPED:
                 grouped_name = GROUPED[activity["name"]]
-                if grouped_name in summary:
+            else:
+                grouped_name = activity["name"]
+            if grouped_name in summary:
+                if grouped_name not in NON_WORKSHOP_ACTIVITIES:
+                    summary[grouped_name] += activity["allocation_count"]
+                    print(f"Adding {activity['allocation_count']} to {grouped_name}")
+                else:
                     # We don't want to sum things, just record the value once
                     # But log the old value
                     print(
                         f"Warning: {grouped_name} already in summary with value {summary[grouped_name]}, overwriting with {activity['allocation_count']}"
                     )
                     summary[grouped_name] = activity["allocation_count"]
-
-                else:
-                    summary[grouped_name] = activity["allocation_count"]
             else:
-                summary[activity["name"]] = activity["allocation_count"]
+                summary[grouped_name] = activity["allocation_count"]
 
         else:
-            summary["Workshops"] += activity["allocation_count"]
-            workshop_activity_names.add(activity["name"])
+            summary["Other"] += activity["allocation_count"]
+            unknown_activity_names.add(activity["name"])
 
-    print("Non-workshop activities found:", ", ".join(sorted(workshop_activity_names)))
+    print("Unknown activities found:", ", ".join(sorted(unknown_activity_names)))
+
+    # Sort the summary by key
+    summary = dict(sorted(summary.items()))
 
     return summary
 
